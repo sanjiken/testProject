52ac154252f6dc8be601f64de362b8f6c90cb3d1
nader <touibi.nader@gmail.com>
2017-01-27 09:07:09 -0500

    test 1

---
+++ TestGitProject/src/demo/Test2.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Test2 {
+
+	public String testMethed(){
+		
+		return "Helloooo !!! ";
+	}
+	
+}
---
+++ TestGitProject/src/demo/testClass.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("coucou here i am ");
+		System.out.println("coucou here i am "); 
+		
+		
+		
+		String str = new Test2().testMethed();
+		
+		System.out.println("coucou here i am " + str); 
+		
+	}
+
+}
