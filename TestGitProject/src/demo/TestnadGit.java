1bb9bccd60f1675fe70c8963e239bd136f1dfe3b
radh06 <seyesradhouene@gmail.com>
2017-01-30 08:37:44 +0100

    commuit

--- TestGitProject/src/demo/TestnadGit.java
+++ TestGitProject/src/demo/TestnadGit.java
@@ -6,7 +6,11 @@ public class TestnadGit {
 	public void ajoutTest() {
 		
 		System.out.println("test nad ajout"); 
+		System.out.println("a new test");
 		
 	}
 	
+	public void ajoutAnotherRound(){
+		String s ="hello";
+	}
 }
