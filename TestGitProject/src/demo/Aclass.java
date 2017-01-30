b2b595ccb95be83779e0ee4643d8095fec2f08dd
nader <touibi.nader@gmail.com>
2017-01-30 09:51:57 -0500

    aaaa

--- TestGitProject/src/demo/Aclass.java
+++ TestGitProject/src/demo/Aclass.java
@@ -1,9 +1,10 @@
 package demo;
-
-
+//github.com/sanjiken/testProject/
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import model.Person;
 
 public class Aclass {
 
@@ -20,13 +21,14 @@ public class Aclass {
 		
 		String a1 = "just for test";
 		String a2 = "just for test 2";
+		String a3 = a1+a2;
 		if (a1.equals("jjjj")) {
 			a1 = "llllll";
 		}else{
 			a1 = "222222";
 		}
 		String k = "kadhé";
-		return "a new class";
+		return a3;
 		
 
 	}
@@ -42,6 +44,19 @@ public class Aclass {
 			System.out.println("this is your g : " + g);
 		}
 		
+		Person p = new Person();
+		
+		p.setNom("nader");
+		
+		
+		
+		return (b*f);
+	}
+	public float essai23(){
+		List<TestnadGit> tag = new ArrayList<TestnadGit>();
+		tag.add(new TestnadGit());
+		float b = 478;
+		float f = 1545;
 		return (b*f);
 	}
 		
---
+++ TestGitProject/src/model/Person.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author pc
+ *
+ */
+public class Person {
+
+	String nom ;
+	String prenom;
+	String phone;
+	public Person() {
+		super();
+	}
+	public Person(String nom, String prenom, String phone) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.phone = phone;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
