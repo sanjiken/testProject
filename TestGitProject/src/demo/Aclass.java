e0438fdf4558299aa1faf82635aeb51e502512fe
nader <touibi.nader@gmail.com>
2017-01-30 08:42:48 -0500

    test commit 45

--- TestGitProject/src/demo/Aclass.java
+++ TestGitProject/src/demo/Aclass.java
@@ -3,10 +3,24 @@ package demo;
 public class Aclass {
 
 	public Test2 essai(){
+		String a1 = "just for test";
+		int n = 100;
 		return new Test2();
 	}
 
 	public String essai2(){
+		
+		String a1 = "just for test";
+		if (a1.equals("jjjj")) {
+			a1 = "llllll";
+		}
 		return "a new class";
+		
 	}
+	public float essai22(){
+		float b = 478;
+		float f = 1545;
+		return (b*f);
+		
+	
 }
---
+++ TestGitProject/src/demo/secondClassTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author nader
+ *
+ */
+public class secondClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String just = "just for testing";
+		
+	}
+
+}
