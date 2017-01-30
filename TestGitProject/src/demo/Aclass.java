723c4845410272590631b6cafb5a6bcf516c6210
radh06 <seyesradhouene@gmail.com>
2017-01-30 08:40:51 +0100

    azesdqaeasd

--- TestGitProject/src/demo/Aclass.java
+++ TestGitProject/src/demo/Aclass.java
@@ -3,10 +3,13 @@ package demo;
 public class Aclass {
 
 	public Test2 essai(){
+		int n = 100;
 		return new Test2();
 	}
 
-	public String essai2(){
-		return "a new class";
+	public float essai2(){
+		float b = 478;
+		float f = 1545;
+		return (b*f);
 	}
 }
