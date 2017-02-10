1ae567b44abf010d54d4d1d7ea07ea3ac8598eb4
nader <nader@WIN-6V7H1KKJ069>
2017-02-10 17:20:27 +0100

    vvvvvvv

---
+++ TestGitProject/src/demo/Gclass.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Gclass {
+
+	
+	/*
+	 * testtesttest
+	 */
+	
+}
--- TestGitProject/src/demo/thirdClass.java
+++ TestGitProject/src/demo/thirdClass.java
@@ -5,10 +5,21 @@ public class thirdClass {
 	public void testMethod(){
 		
 		System.out.print("just for testing");
+		System.out.print("just for testing nader");
+		System.out.print("just for testing ffff");
+		System.out.print("just for testing fffff");
+		System.out.print("just for testing gggggg");
 		System.out.print("just for testing arij");
 		System.out.print("just for testing nader");
 		
 		System.out.print("just for testing ariij 22222");
+		
+		
+		
+		System.out.print("just for testing ariij rrrrr");System.out.print("just for testing ariij gggggg");
+		System.out.print("just for testing ariij vvvv");
+		
+		
 	}
 	
 }
