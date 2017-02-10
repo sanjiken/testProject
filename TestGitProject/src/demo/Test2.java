36b27d3f8c7a3b1014ebefe594b889338728765b
nader <nader@WIN-6V7H1KKJ069>
2017-02-10 17:44:48 +0100

    ddddd

--- TestGitProject/src/demo/Test2.java
+++ TestGitProject/src/demo/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public String testMethed(){
 	
 		System.out.println("coucou here i am radh"); 
+		System.out.println("coucou here i am arij"); 
+		System.out.println("coucou here i am nad"); 
 		
 		return "Helloooo !!! ";
 	}
