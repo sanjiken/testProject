c52c1597ee43f84b66b5eec1b487cca7a7ec2941
nader <touibi.nader@gmail.com>
2017-02-10 11:14:00 -0500

    vvvvvv

--- TestGitProject/src/demo/thirdClass.java
+++ TestGitProject/src/demo/thirdClass.java
@@ -6,6 +6,9 @@ public class thirdClass {
 		
 		System.out.print("just for testing");
 		System.out.print("just for testing nader");
+		System.out.print("just for testing ffff");
+		System.out.print("just for testing fffff");
+		System.out.print("just for testing gggggg");
 	}
 	
 }
