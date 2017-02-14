d7e64e90ae1178d012f591b802902674977c517a
nader <touibi.nader@gmail.com>
2017-02-14 08:45:59 -0500

    bbbbb

--- TestGitProject/src/demo/Gclass.java
+++ TestGitProject/src/demo/Gclass.java
@@ -19,6 +19,11 @@ public class Gclass {
 		// TODO Auto-generated method stub
 		return super.clone();
 	}
+	
+	protected Object ffffff() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
 
 	@Override
 	public String toString() {
